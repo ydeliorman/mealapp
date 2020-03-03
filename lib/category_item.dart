221b520695f58650a0b2121263047802f0d62c3a
Yalin Eren Deliorman <yalin.deliorman@gmail.com>
2020-03-03 23:09:34 +0300

    First commit of Meal app

---
+++ lib/category_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shopapp/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+
+  CategoryItem(this.id, this.title, this.color);
+
+  /// Add route to CategoryMealsScreen. Use "_" if no parameter is needed
+  void selectCategory(BuildContext ctx) {
+    //push the data to ModalRoute.of(context).settings.arguments using "/category-meals" name
+    Navigator.of(ctx).pushNamed(CategoryMealsScreen.routeName, arguments: {
+      'id': id,
+      'title': title,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //GestureDetector with effects
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(5),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = 'category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    //find every meal that matches with categoryId
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      //ListView.builder is for better performance instead of children
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
